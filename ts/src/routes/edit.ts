58a56c73a35520469001dd889242f43c6763cecf
juukuzi <juukuzi@gmail.com>
2017-10-25 19:53:57 +0900

    add: use connect-ensure-login

--- ts/src/routes/edit.ts
+++ ts/src/routes/edit.ts
@@ -1,22 +1,20 @@
 import { Request, Response, Router } from 'express';
-import * as passport from 'passport';
+import { ensureLoggedIn } from "connect-ensure-login";
 import World from "../scraping/World";
 import Category from "../scraping/Category";
 
 const edit = Router();
 
 edit.get('/',
-    passport.authenticate(
-        'twitter',
-        (req: Request, res: Response) => {
-            res.render('edit', {
-                title: 'Edit',
-                user: req.user,
-                world: [...World.map.keys()],
-                category: [...Category.map.keys()]
-            });
-        }
-    )
+    ensureLoggedIn(),
+    (req: Request, res: Response) => {
+        res.render('edit', {
+            title: 'Edit',
+            user: req.user,
+            world: [...World.map.keys()],
+            category: [...Category.map.keys()]
+        });
+    }
 );
 
 export default edit;
